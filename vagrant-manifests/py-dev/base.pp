d480e2b5e4fd8c72ac4136720bf738e81ffb75ad
Seb Insua <IM.Hanz@gmail.com>
2012-10-04 11:04:46 +0100

    Changing to using Chef for configuration management...

--- vagrant-manifests/py-dev/base.pp
+++
@@ -1,9 +0,0 @@
-File { owner => 0, group => 0, mode => 0644 }
-
-file { '/etc/motd':
-    content => "Welcome to your Vagrant-built Python virtual machine!\nManaged by Puppet.\n"
-}
-
-node default {
-    include python
-}
